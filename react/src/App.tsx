d1a7707ebc9678d9d941104632816f0450444877
renatabfs <renata.niall66@gmail.com>
2022-09-14 08:42:52 -0300

    Primeiro commit React

--- react/src/App.tsx
+++ react/src/App.tsx
@@ -1,6 +1,23 @@
+interface ButtonProps {
+  title?: string;
+  color?: string;
+}
+
+
+function Button (props: ButtonProps) {
+  return (
+    <button>{props.title}</button>
+  )
+}
+
 
 function App() {
-  return <h1> Hello world</h1> 
+  return (
+    <div>
+  <h1>Hello Word</h1>
+      <Button title="Enviar 1"/>
+  </div>
+  )
   }
 
 export default App
