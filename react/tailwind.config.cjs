b53b9af9f4129e898ba12f189fe6b49631a8d4ee
renatabfs <renata.niall66@gmail.com>
2022-09-20 08:07:01 -0300

    Imagem de fundo e body adicionados

--- react/tailwind.config.cjs
+++ react/tailwind.config.cjs
@@ -1,8 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/**/*.tsx'],
+  content: ['./src/**/*.tsx',
+            './index.html',],
   theme: {
-    extend: {},
+    extend: {
+      colors: { 
+       
+      },
+      backgroundImage: {
+      'nlw-gradient': 'linear-gradient(89.86deg, #9572FC 27.08%, #43E7AD 40.94%, #E1D55D 40.57%);',
+        
+        background: "url('/Fundo.svg')",
+      'footer-texture': "url('/img/footer-texture.png')",
+      }
+    },
   },
   plugins: [],
 }
