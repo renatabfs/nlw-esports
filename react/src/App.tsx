fe7e63aea99563e15608e74e02a2fb4252016f77
renatabfs <renata.niall66@gmail.com>
2022-09-20 08:08:06 -0300

    imagens adicionadas à landing page

--- react/src/App.tsx
+++ react/src/App.tsx
@@ -1,23 +1,34 @@
 import './styles/main.css'
-
-interface ButtonProps {
-  title?: string;
-  color?: string;
-}
-
-
-function Button (props: ButtonProps) {
-  return (
-    <button>{props.title}</button>
-  )
-}
+import logoImg from './assets/logo.svg'
 
 
 function App() {
   return (
-    <div className='w-8 h-8 bg-black dark:bg-red-600'>
+  <div className="flex flex-col items-center justify-center max-w-[1344px] mx-auto my-20">
+    <img src={logoImg} alt='logo' />
+      <h1 className="text-6xl text-white font-black mt-20">Seu <span className='text-transparent bg-nlw-gradient bg-clip-text'> duo </span> está aqui.</h1>
+      
+      <div className='grid grid-cols-6 gap-6 mt-16'>
+      <a href='' >
+        <img src="/game1.svg" alt ="" />
+      </a>
+        <a href='' >
+          <img src="/game2.svg" alt="" />
+        </a>
+        <a href='' >
+          <img src="/game3.svg" alt="" />
+        </a>
+        <a href='' >
+          <img src="/game4.svg" alt="" />
+        </a>
+        <a href='' >
+          <img src="/game5.svg" alt="" />
+        </a>
+        <a href='' >
+          <img src="/game6.svg" alt="" />
+        </a>
+      </div>
   </div>
   )
   }
-
 export default App
