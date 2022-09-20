c2ba1c10374b628a644aff896f65b45155f56da5
renata <renata.niall66@gmail.com>
2022-09-20 11:42:57 +0000

    Anúncios de duo adicionado

--- react/src/App.tsx
+++ react/src/App.tsx
@@ -1,5 +1,5 @@
 import './styles/main.css'
-import logoImg from './assets/logo.svg'
+import logoImg from './assets/Logo.svg'
 
 
 function App() {
@@ -9,8 +9,13 @@ function App() {
       <h1 className="text-6xl text-white font-black mt-20">Seu <span className='text-transparent bg-nlw-gradient bg-clip-text'> duo </span> está aqui.</h1>
       
       <div className='grid grid-cols-6 gap-6 mt-16'>
-      <a href='' >
+      <a href='' className='relative rounded-lg overflow-hidden'>
         <img src="/game1.svg" alt ="" />
+
+      <div className='w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 right-0'> 
+      <strong className ="font-bold text-white block">League of legends</strong>
+      <span className='text-zinc-300 text-sm block mt-1'>5 anúncios</span>
+      </div>
       </a>
         <a href='' >
           <img src="/game2.svg" alt="" />
