6c4c1a08e78e36636f46edd63825526c827222fb
renata <renata.niall66@gmail.com>
2022-09-20 11:42:22 +0000

    Gradient de anúncios adicionado

--- react/tailwind.config.cjs
+++ react/tailwind.config.cjs
@@ -9,7 +9,7 @@ module.exports = {
       },
       backgroundImage: {
       'nlw-gradient': 'linear-gradient(89.86deg, #9572FC 27.08%, #43E7AD 40.94%, #E1D55D 40.57%);',
-        
+      'game-gradient': 'linear-gradient(180deg, rgba(0, 0, 0, 0) 0%, rgba(0, 0, 0, 0.9) 67.08%)',
         background: "url('/Fundo.svg')",
       'footer-texture': "url('/img/footer-texture.png')",
       }
