53f6eeaa29ddb4d89a92c9a44c829f12d489b872
renata <renata.niall66@gmail.com>
2022-09-20 13:49:55 +0000

    box de publicar anúncio criado

--- react/src/App.tsx
+++ react/src/App.tsx
@@ -1,4 +1,5 @@
 import './styles/main.css'
+import {MagnifyingGlassPlus} from 'phosphor-react'
 import logoImg from './assets/Logo.svg'
 
 
@@ -47,10 +48,21 @@ function App() {
         <a href='' className='relative rounded-lg overflow-hidden' >
           <img src="/game6.svg" alt="" />
           <div className='w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 right-0'>
-            <strong className="font-bold text-white block">World Warcraft</strong>
+            <strong className="font-bold text-white block">World of Warcraft</strong>
             <span className='text-zinc-300 text-sm block mt-1'>4 anúncios</span>
           </div>
         </a>
+      </div>
+      <div className='pt-1 bg-nlw-gradient self-stretch rounded-lg overflow-hidden mt-8'>
+      <div className='bg-[#2A2634] px-8 py-6 flex justify-between items-center'>
+      <div>
+      <strong className='text-2xl text-white font-black block'>Não encontrou seu duo?</strong>
+      <span className='text-zinc-400 block'> Publique um anúncio para encontrar mais players!</span>
+      </div>
+      <button className='py-3 px-4 text-white bg-violet-500 hover:bg-violet-600 flex items-center gap-3'>
+        <MagnifyingGlassPlus size={24}/>
+        Publicar anúncio</button>
+      </div>
       </div>
     </div>
   )
