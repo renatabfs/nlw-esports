ef5d07c511456c728993843a7ed4da47cd5ba573
renatabfs <renata.niall66@gmail.com>
2022-09-23 08:25:46 -0300

    Arquivos adicionados

---
+++ reactnative/src/screens/Home/index.tsx
@@ -0,0 +1,28 @@
+import { FlatList, Image, View } from "react-native";
+
+import logoImg from "../../assets/logo-nlw-esports.png";
+import { GameCard } from "../../components/GameCard";
+import { Heading } from "../../components/Heading";
+import { GAMES } from "../../utils/games";
+
+import { styles } from "./styles";
+
+export function Home() {
+  return (
+    <View style={styles.container}>
+      <Image source={logoImg} style={styles.logo} />
+      <Heading
+        title="Encontre seu duo!"
+        subTitle="Selecione o game que deseja jogar..."
+      />
+      <FlatList
+        data={GAMES}
+        contentContainerStyle={styles.contentList}
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        keyExtractor={(item) => item.id}
+        renderItem={({ item }) => <GameCard data={item} />}
+      />
+    </View>
+  );
+}
