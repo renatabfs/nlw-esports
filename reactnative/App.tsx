700c6fb7fe3f598643c53f09fcfef0c72a13d4e3
renatabfs <renata.niall66@gmail.com>
2022-09-23 09:22:57 -0300

    fontes adicionadas

--- reactnative/App.tsx
+++ reactnative/App.tsx
@@ -1,32 +1,33 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
-
-interface ButtonProps {
-  title?: string;
-}
-
-function Button(props: ButtonProps) {
-  return (
-    <TouchableOpacity style={styles.button}>
-      <Text style={styles.buttonText}>{props.title}</Text>
-    </TouchableOpacity>
-  )
-}
+import React from 'react';
+import { Background } from './src/components/Background';
+import {
+  useFonts,
+  Inter_400Regular,
+  Inter_600SemiBold,
+  Inter_700Bold,
+  Inter_900Black
+} from '@expo-google-fonts/inter'
+import { Home } from './src/screens/Home';
+import { Loading } from './src/components/Loading';
 
 export default function App() {
+  const [fontsLoaded] = useFonts ({
+    useFonts,
+  Inter_400Regular,
+  Inter_600SemiBold,
+  Inter_700Bold,
+  Inter_900Black
+  });
+
   return (
-    <View style={styles.container}>
-      <Button title ="Butão" />
-      <StatusBar style="auto" />
-    </View>
+      <Background>
+      <StatusBar style="light"
+      backgroundColor='transparent'
+      />
+    {fontsLoaded ? <Home/> : <Loading /> }
+      </Background>
+  
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
