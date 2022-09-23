6f8e0e3538f582876b9ab0cb90b5e9a893cabefd
renatabfs <renata.niall66@gmail.com>
2022-09-23 09:34:38 -0300

    Logo e header adicionados na tela home

--- reactnative/src/screens/Home/index.tsx
+++ reactnative/src/screens/Home/index.tsx
@@ -1,27 +1,19 @@
 import { FlatList, Image, View } from "react-native";
-
-import logoImg from "../../assets/logo-nlw-esports.png";
-import { GameCard } from "../../components/GameCard";
+import React from "react";
+import logoImg from '../../assets/logo-nlw-esports.png'
 import { Heading } from "../../components/Heading";
-import { GAMES } from "../../utils/games";
-
 import { styles } from "./styles";
 
 export function Home() {
   return (
     <View style={styles.container}>
-      <Image source={logoImg} style={styles.logo} />
-      <Heading
-        title="Encontre seu duo!"
-        subTitle="Selecione o game que deseja jogar..."
+      <Image source = {logoImg}
+      style = {styles.logo}
       />
-      <FlatList
-        data={GAMES}
-        contentContainerStyle={styles.contentList}
-        horizontal
-        showsHorizontalScrollIndicator={false}
-        keyExtractor={(item) => item.id}
-        renderItem={({ item }) => <GameCard data={item} />}
+
+      <Heading 
+      title="Encontre seu duo!"
+      subTitle="Selecione o game que deseja jogar..."
       />
     </View>
   );
