5f2963bd576760f1e0d8fc79319ea3e90bd155c2
renatabfs <renata.niall66@gmail.com>
2022-09-25 20:08:47 -0300

    Tabela conectada ao banco de dados sql studio

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,18 +1,30 @@
 import express, { response } from 'express';
+import {PrismaClient} from '@prisma/client'
 
 const app = express ()
+const prisma = new PrismaClient ({
+      log: ['query']
+})
 
-app.get('games', (request, response) => {
-   return response.json([]);
+app.get('/games', async (request, response) => {
+   const games = await prisma.game.findMany({
+      include: {
+         _count: {
+            select: {
+               Ad: true,
+            }  
+         }
+      }
+   })
+   return response.json(games);
 })
 
 app.post('/ads', (request, response) => {
    return response.status(201).json([]);
 })
-
 app.get('/games/:id/ads', (request, response) => {
-   // const gameId = request.params.id;
-   // return response.send(gameId);
+   const gameId = request.params.id;
+   
    return response.json([
     {id: 1, name: 'Ad 1'},
     {id: 2, name: 'Ad 2'},
