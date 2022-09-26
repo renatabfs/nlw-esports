251155a15703342f0a46ae5880cae2b9785ba54f
renata <renata.niall66@gmail.com>
2022-09-26 23:41:19 +0000

    Adicionando os componentes

--- react/src/App.tsx
+++ react/src/App.tsx
@@ -1,7 +1,7 @@
 import './styles/main.css'
 import {MagnifyingGlassPlus} from 'phosphor-react'
 import logoImg from './assets/Logo.svg'
-
+import { GameBanner } from './components/GameBanner'
 
 function App() {
   return (
@@ -10,48 +10,13 @@ function App() {
       <h1 className="text-6xl text-white font-black mt-20">Seu <span className='text-transparent bg-nlw-gradient bg-clip-text'> duo </span> está aqui.</h1>
 
       <div className='grid grid-cols-6 gap-6 mt-16'>
-        <a href='' className='relative rounded-lg overflow-hidden'>
-          <img src="/game1.svg" alt="" />
-          <div className='w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 right-0'>
-            <strong className="font-bold text-white block">League of legends</strong>
-            <span className='text-zinc-300 text-sm block mt-1'>5 anúncios</span>
-          </div>
-        </a>
-        <a href='' className='relative rounded-lg overflow-hidden'>
-          <img src="/game2.svg" alt="" />
-          <div className='w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 right-0'>
-            <strong className="font-bold text-white block">DOTA 2</strong>
-            <span className='text-zinc-300 text-sm block mt-1'>3 anúncios</span>
-          </div>
-        </a>
-        <a href='' className='relative rounded-lg overflow-hidden'>
-          <img src="/game3.svg" alt="" />
-          <div className='w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 right-0'>
-            <strong className="font-bold text-white block">Counter Strike</strong>
-            <span className='text-zinc-300 text-sm block mt-1'>7 anúncios</span>
-          </div>
-        </a>
-        <a href='' className='relative rounded-lg overflow-hidden'>
-          <img src="/game4.svg" alt="" />
-          <div className='w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 right-0'>
-            <strong className="font-bold text-white block">APEX Legends</strong>
-            <span className='text-zinc-300 text-sm block mt-1'>1 anúncio</span>
-          </div>
-        </a>
-        <a href='' className='relative rounded-lg overflow-hidden' >
-          <img src="/game5.svg" alt="" />
-          <div className='w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 right-0'>
-            <strong className="font-bold text-white block">Fortinite</strong>
-            <span className='text-zinc-300 text-sm block mt-1'>8 anúncios</span>
-          </div>
-        </a>
-        <a href='' className='relative rounded-lg overflow-hidden' >
-          <img src="/game6.svg" alt="" />
-          <div className='w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 right-0'>
-            <strong className="font-bold text-white block">World of Warcraft</strong>
-            <span className='text-zinc-300 text-sm block mt-1'>4 anúncios</span>
-          </div>
-        </a>
+        <GameBanner bannerUrl="./game1.svg" adsCount= {5} title ='League Of Legends'/>
+        <GameBanner bannerUrl="./game2.svg" adsCount= {2} title ='DOTA 2'/>
+        <GameBanner bannerUrl="./game3.svg" adsCount= {7} title ='CS:GO'/>
+        <GameBanner bannerUrl="./game4.svg" adsCount= {1} title ='Apex Legends'/>
+        <GameBanner bannerUrl="./game5.svg" adsCount= {6} title ='Fortinite'/>
+        <GameBanner bannerUrl="./game6.svg" adsCount= {3} title ='World of Warcraft'/>
+
       </div>
       <div className='pt-1 bg-nlw-gradient self-stretch rounded-lg overflow-hidden mt-8'>
       <div className='bg-[#2A2634] px-8 py-6 flex justify-between items-center'>
