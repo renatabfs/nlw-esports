afae3f8ae088a87ba56ccc84f36e62f6563f83eb
renata <renata.niall66@gmail.com>
2022-09-26 23:47:42 +0000

    Componente adicionado à tela

--- react/src/App.tsx
+++ react/src/App.tsx
@@ -1,7 +1,7 @@
 import './styles/main.css'
-import {MagnifyingGlassPlus} from 'phosphor-react'
 import logoImg from './assets/Logo.svg'
 import { GameBanner } from './components/GameBanner'
+import { CreateAdBanner } from './components/CreateAdBanner'
 
 function App() {
   return (
@@ -18,17 +18,7 @@ function App() {
         <GameBanner bannerUrl="./game6.svg" adsCount= {3} title ='World of Warcraft'/>
 
       </div>
-      <div className='pt-1 bg-nlw-gradient self-stretch rounded-lg overflow-hidden mt-8'>
-      <div className='bg-[#2A2634] px-8 py-6 flex justify-between items-center'>
-      <div>
-      <strong className='text-2xl text-white font-black block'>Não encontrou seu duo?</strong>
-      <span className='text-zinc-400 block'> Publique um anúncio para encontrar mais players!</span>
-      </div>
-      <button className='py-3 px-4 text-white bg-violet-500 hover:bg-violet-600 flex items-center gap-3'>
-        <MagnifyingGlassPlus size={24}/>
-        Publicar anúncio</button>
-      </div>
-      </div>
+      <CreateAdBanner />
     </div>
   )
 }
