7ee51fb103f9d47360f12dba0e5fa269ee2f417b
renata <renata.niall66@gmail.com>
2022-09-27 14:25:04 +0000

    Estilizando os Inputs

--- react/src/App.tsx
+++ react/src/App.tsx
@@ -58,32 +58,37 @@ function App() {
 
       <Dialog.Content className='fixed bg-[#2A2634] py-8 px-10 text-white top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 rounded-lg w-[480px] shadow-lg shadow-black/25'>
       <Dialog.Title className='text-3xl font-black'>Publique um anúncio</Dialog.Title>
-       <form className='mt-8'>
+       <form className='mt-8 flex flex-col gap-4'>
+
         <div className='flex flex-col gap-2'>
         <label htmlFor='game' className='font-semibold' > Qual o game?</label>
         <Input 
         id = 'game' 
         placeholder='Selecione o game que deseja jogar'/>
+        </div>
+
+        <div className='flex flex-col gap-2'>
         <label htmlFor='name'> Seu nome (ou nickname)</label>
         <Input id = 'name' placeholder='Como te chamam dentro do jogo?'/>
         </div>
-        <div>
-          <div className=''>
+        
+        <div className='grid grid-cols-2 gap-6'>
+          <div className='flex flex-col gap-2'>
           <label htmlFor='yearsPlaying'>Joga há quantos anos?</label>
           <Input id ='yearsPlaying' type="number" placeholder ="Tudo bem ser ZERO" />
           </div>
-          <div>
+          <div className='flex flex-col gap-2'>
           <label htmlFor='discord'>Qual seu discord?</label>
           <Input id = 'discord' type="text" placeholder='usuário#0000' />
           </div>
         </div>
-        <div>
-          <div>
+        <div className='flex gap-6'>
+          <div className='flex flex-col gap-2'>
             <label htmlFor='weekDays'>Quando costuma jogar?</label>
           </div>
-          <div>
+          <div className='flex flex-col gap-2 flex-1'>
             <label htmlFor='hourStart'>Qual horário do dia?</label>
-            <div>
+            <div className='grid grid-cols-2 gap-1'>
             <Input id ='hourStart' type='time' placeholder='De' />
             <Input id ='hourEnd' type='time' placeholder='Até' />
             </div>
