81d7afebb6faf29b396605ce8ed17bb6c99e8487
renata <renata.niall66@gmail.com>
2022-09-27 00:27:19 +0000

    aplicação conectada a API

--- react/src/App.tsx
+++ react/src/App.tsx
@@ -2,20 +2,46 @@ import './styles/main.css'
 import logoImg from './assets/Logo.svg'
 import { GameBanner } from './components/GameBanner'
 import { CreateAdBanner } from './components/CreateAdBanner'
+import {useState, useEffect} from 'react'
+
+interface Game {
+  title: string
+  bannerUrl: string
+  ads: number
+  id: string
+  _count: {
+    ads:number
+  }
+}
 
 function App() {
+  const [games, setGames] = useState <Game[]>([])
+
+  useEffect (() => {
+    fetch('http://localhost:3333/games')
+    .then (response => response.json())
+    .then (data => {
+      setGames(data)
+    })
+  }, [])
+
   return (
     <div className="flex flex-col items-center justify-center max-w-[1344px] mx-auto my-20">
       <img src={logoImg} alt='logo' />
       <h1 className="text-6xl text-white font-black mt-20">Seu <span className='text-transparent bg-nlw-gradient bg-clip-text'> duo </span> está aqui.</h1>
 
-      <div className='grid grid-cols-6 gap-6 mt-16'>
-        <GameBanner bannerUrl="./game1.svg" adsCount= {5} title ='League Of Legends'/>
-        <GameBanner bannerUrl="./game2.svg" adsCount= {2} title ='DOTA 2'/>
-        <GameBanner bannerUrl="./game3.svg" adsCount= {7} title ='CS:GO'/>
-        <GameBanner bannerUrl="./game4.svg" adsCount= {1} title ='Apex Legends'/>
-        <GameBanner bannerUrl="./game5.svg" adsCount= {6} title ='Fortinite'/>
-        <GameBanner bannerUrl="./game6.svg" adsCount= {3} title ='World of Warcraft'/>
+      <div className='grid grid-cols-6 gap-6 mt-16'> 
+
+      {games.map(game => {
+          return (
+            <GameBanner
+              key={game.id}
+              title={game.title}
+              bannerUrl={game.bannerUrl}
+              adsCount={game._count.ads}
+            />
+          )
+        })}
 
       </div>
       <CreateAdBanner />
