a51d2e338b9ad6f1a8401d181d23a34d06e51e99
renata <renata.niall66@gmail.com>
2022-10-07 11:57:16 +0000

    Botões de submit estilizados

--- react/src/App.tsx
+++ react/src/App.tsx
@@ -125,13 +125,20 @@ function App() {
                   </div>
                 </div>
               </div>
-              <div>
+              <div className='mt-2 flex gap-2 text-small'>
                 <Input type='checkbox' />
                 Costumo me conectar ao chat de voz
               </div>
-              <footer>
-                <button>Cancelar</button>
-                <button type='submit' ><GameController />Encontrar duo</button>
+              <footer className='mt-4 flex justify-end gap-4'>
+                <Dialog.Close
+                  type='button'
+                  className='bg-zinc-500 px-5 h-12 rounded-md font-semibold hover:bg-zinc-600'
+                >Cancelar</Dialog.Close>
+                <button
+                  className='bg-violet-500 px-5 h-12 rounded-md font-semibold flex items-center gap-3 hover:bg-violet-600'
+                  type='submit'
+                ><GameController className='w-6 h-6' />
+                  Encontrar duo</button>
               </footer>
 
             </form>
