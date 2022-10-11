77532cdba82e9310859a07fa04211c6af80f5e9d
renata <renata.niall66@gmail.com>
2022-10-11 11:40:22 +0000

    Fetch para o server adicionado

--- reactnative/src/components/GameCard/index.tsx
+++ reactnative/src/components/GameCard/index.tsx
@@ -9,12 +9,15 @@ import { LinearGradient } from "expo-linear-gradient";
 
 import { styles } from "./styles";
 import { THEME } from "../../theme";
+import React from "react";
 
 export interface GameCardProps {
   id: string;
-  name: string;
-  ads: string;
-  cover: ImageSourcePropType;
+  title: string;
+  _count :{
+    ads: number;
+  }
+  bannerUrl: string;
 }
 
 interface Props extends TouchableOpacityProps {
@@ -26,13 +29,13 @@ export function GameCard({ data, ...rest }: Props) {
     <TouchableOpacity {...rest} style={styles.container}>
       <ImageBackground 
       style={styles.cover} 
-      source={data.cover}>
+      source={{uri: data.bannerUrl}}>
       <LinearGradient
         colors ={THEME.COLORS.FOOTER}
         style= {styles.footer}
       >
-      <Text style={styles.name}>{data.name}</Text>
-      <Text style = {styles.ads}>{data.ads} anúncios</Text>
+      <Text style={styles.name}>{data.title}</Text>
+      <Text style = {styles.ads}>{data._count.ads} anúncios</Text>
       </LinearGradient>
       </ImageBackground>
     </TouchableOpacity>
--- reactnative/src/screens/Home/index.tsx
+++ reactnative/src/screens/Home/index.tsx
@@ -1,11 +1,21 @@
 import { FlatList, Image, View } from "react-native";
 import logoImg from '../../assets/logo-nlw-esports.png'
-import { GameCard } from "../../components/GameCard";
+import { GameCard, GameCardProps } from "../../components/GameCard";
 import { Heading } from "../../components/Heading";
 import { styles } from "./styles";
-import { GAMES } from "../../utils/games";
+import { useEffect, useState } from "react";
+import React from "react";
+
 
 export function Home() {
+  const [games, setGames] = useState<GameCardProps[]>([])
+  
+  useEffect(() =>{
+    fetch('https://3333-renatabfs-nlwesports-k2dmsbz6w22.ws-us70.gitpod.io/games')
+    .then(response => response.json())
+    .then(data => setGames(data))
+  }, [])
+
   return (
     <View style={styles.container}>
       <Image source = {logoImg}
@@ -17,7 +27,7 @@ export function Home() {
       subTitle="Selecione o game que deseja jogar..."
       />
       <FlatList 
-      data = {GAMES}
+      data = {games}
       keyExtractor = {item => item.id}
       renderItem = {({item}) => (
         <GameCard 
